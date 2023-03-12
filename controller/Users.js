313f1ee7d6c4069c9866c4c9aea8017915374096
Fajar Agung <fajar.agngn@gmail.com>
2023-03-12 22:19:12 +0700

    CRUD : DELETE

--- controller/Users.js
+++ controller/Users.js
@@ -1,14 +1,18 @@
-const getAllUsers = (req, res) => {
-  const dataDummy = {
-    nama: "Fajar Agung",
-    nim: "312010448",
-    kelas: "TI.20.A.3"
-  }
-  res.json({
-    message: 'GET all users success!',
-    data: dataDummy
-  })
+const dataDummy = {
+  id: 1,
+  nama: "Fajar Agung",
+  nim: "312010448",
+  kelas: "TI.20.A.3"
 }
+
+// const getUserById = (req, res) => {
+//   const { id } = req.params
+//   console.log(req.body)
+//   res.json({
+//     message: 'READ success',
+//     data: req.body
+//   })
+// }
 
 const createNewUsers = (req, res) => {
   console.log(req.body);
@@ -18,7 +22,39 @@ const createNewUsers = (req, res) => {
   })
 }
 
+const getAllUsers = (req, res) => {
+  res.json({
+    message: 'GET all users success!',
+    data: dataDummy
+  })
+}
+
+const updateUser = (req, res) => {
+  const { id } = req.params
+  console.log('id user:', id)
+  res.json({
+    message: 'UPDATE success',
+    data: req.body
+  })
+}
+
+const deleteUser = (req, res) => {
+  const { id } = req.params
+  console.log('id user:', id)
+  res.json({
+    message: 'DELETE success',
+    data: {
+      id: id,
+      nama: 'fajar',
+      nim: '312010448',
+      kelas: 'A1'
+    }
+  })
+}
+
 module.exports = {
   getAllUsers,
-  createNewUsers
+  createNewUsers,
+  updateUser,
+  deleteUser
 }
--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,19 @@ const express = require('express')
 const userController = require('../controller/Users')
 const router = express.Router()
 
+// CREATE - POST
+router.post('/', userController.createNewUsers)
+
 // READ - GET
 router.get('/', userController.getAllUsers)
 
-// CREATE - POST
-router.post('/', userController.createNewUsers)
+// READ - GET by id
+// router.get('/:id', userController.getUserById)
+
+// UPDATE - PATCH
+router.patch('/:id', userController.updateUser)
+
+// UPDATE - PATCH
+router.delete('/:id', userController.deleteUser)
 
 module.exports = router;
