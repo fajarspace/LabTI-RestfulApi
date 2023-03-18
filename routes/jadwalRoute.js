55d0d2d55500c6a9f84e9baa58ac79a29741d2e5
Fajar Agung <fajar.agngn@gmail.com>
2023-03-18 12:52:47 +0700

    add jadwal controller

--- routes/jadwalRoute.js
+++ routes/jadwalRoute.js
@@ -1,21 +1,22 @@
 import express from "express";
 import { getJadwal, getJadwalById, createJadwal, updateJadwal, deleteJadwal } from "../controller/Jadwal.js";
+import { verifyUser, adminOnly } from "../middleware/AuthUser.js";
 
 const router = express.Router()
 
 // CREATE - POST
-router.post('/jadwal', createJadwal)
+router.post('/jadwal', verifyUser, createJadwal)
 
 // READ - GET
-router.get('/jadwal', getJadwal)
+router.get('/jadwal', verifyUser, getJadwal)
 
 // READ - GET by id
-router.get('/jadwal/:id', getJadwalById)
+router.get('/jadwal/:id', verifyUser, getJadwalById)
 
 // UPDATE - PATCH
-router.patch('/jadwal/:id', updateJadwal)
+router.patch('/jadwal/:id', verifyUser, updateJadwal)
 
 // DELETE
-router.delete('/jadwal/:id', deleteJadwal)
+router.delete('/jadwal/:id', verifyUser, deleteJadwal)
 
 export default router;
