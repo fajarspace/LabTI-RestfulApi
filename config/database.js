bb1cdd01aa153d4c37a436afccdeb507c5ced58c
Fajar Agung <fajar.agngn@gmail.com>
2023-03-19 13:47:51 +0700

    add .env

--- config/database.js
+++ config/database.js
@@ -1,7 +1,10 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config();
 
-const db = new Sequelize('elab', 'root', '', {
-  host: "localhost",
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: "mysql"
 });
 
