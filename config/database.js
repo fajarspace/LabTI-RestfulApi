347e9388d815d63d635cfef813601833d851cf25
Fajar Agung <fajar.agngn@gmail.com>
2023-03-19 15:15:08 +0700

    finishing

--- config/database.js
+++ config/database.js
@@ -1,16 +1,7 @@
-// import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 dotenv.config();
 
-
-// const db = new Sequelize(process.env.MYSQL_ADDON_DB, process.env.MYSQL_ADDON_USER, process.env.DB_MYSQL_ADDON_PASSWORD, {
-//   host: process.env.MYSQL_ADDON_HOST,
-//   dialect: "mysql"
-// });
-
-// export default db;
-
-import { Sequelize } from "sequelize";
 const db = new Sequelize(process.env.CLEVERCLOUD_MYSQL_DB, process.env.CLEVERCLOUD_MYSQL_USER, process.env.CLEVERCLOUD_MYSQL_PASSWORD, {
   host: process.env.CLEVERCLOUD_MYSQL_HOST,
   dialect: "mysql"
